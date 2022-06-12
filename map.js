5f0befbdf7db0f40fb705e7246f15a4f6fc65f64
Nao Hidaka Kato <89119005+naokatoh@users.noreply.github.com>
2022-06-12 16:04:07 -0400

    Add files via upload

--- map.js
+++ map.js
@@ -1,156 +1,198 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibmsyOTcwIiwiYSI6ImNreDR4ZTZ4dDBhbngydnF1dzBxNzJvMDkifQ.GXAfMWbXTZ7FOAj3rI2oIg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/nk2970/cl3ykyyqn002815p48ctk0pd3',
-    zoom: 3.5,
-    maxZoom:9,
+    style: 'mapbox://styles/nk2970/cl4aq1o1l000k15mzupaicgsl',
+    zoom: 10.6,
+    maxZoom:18,
     minZoom:3,
-    center: [-100.5, 37.7]
+    center: [-73.991, 40.748]
 });
 
 // map1
 map.on("load", function () {
     map.addLayer(
       {
-        id: "County_Typology_Codes",
-        type: "line",
-        source: {
-          type: "geojson",
-          data: "data/countyTypologyCodes.geojson",
-        },
-        paint: {
-          "line-color": "#ffffff",
-          "line-width": 0.05,
-        },
-      },
-      "waterway" // Here's where we tell Mapbox where to slot this new layer
-    ); 
-  
-    map.addLayer(
-      {
-        id: "pop_loss",
-        type: "fill",
-        source: {
-          type: "geojson",
-          data: "data/countyTypologyCodes.geojson",
-        },
-        // maxzoom: 6,
-        paint: {
-          "fill-color": [
-            "match",
-            ["get", "Pop_Loss_2010"],
-            0, "#f1b8b8", //pink
-            1, "#dc4e4e", //red
-            "#ffffff",
-          ],
-          "fill-outline-color": "#ffffff",
-          "fill-opacity": 0.7,
-      },
+        id: "citibike2020",
+        type: "circle",
+    source: {
+      type: "geojson",
+      data: "data/citibikeSep2020.geojson",
     },
- "County_Typology_Codes"
- 
-);
+    paint: {
+      'circle-radius': {
+        stops: [[10, 3], [16, 12]]
+        },
+        "circle-color":
+        [
+          "interpolate",
+          ["linear"],
+          ["get", "tripCount_2020_diff"],
+          -900,
+          "#008837",
+          -17,
+          "#b8e186",
+          -4,
+          "#f2d0e5",
+          8,
+          "#d64fa2"
+
+        ],
+        
+        
+        "circle-stroke-color": "#ffffff",
+        "circle-stroke-width": 0.2,
+        
+        "circle-opacity":1,
+      },
+      },
+
+      "waterway-label" 
+    // Here's where we tell Mapbox where to slot this new layer
+    ); 
+    
+
+  });
+
+
+
+ // Create the popup
+ map.on('click', 'citibike2020', function (e) {
+  var stationName = e.features[0].properties.station_name;
+  var tripStart = e.features[0].properties.tripCount_start2020;
+  var tripEnd = e.features[0].properties.tripCount_end2020;
+  var tripDiff = e.features[0].properties.tripCount_2020_diff;
+  new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<h4>'+stationName+'</h4>'
+          +'<h2>Difference: '+tripDiff+'</h2>'
+          + '<p>Starting trip#: '+tripStart+'<br>'
+          +"Ending trip#: " + tripEnd + '</p>')
+      .addTo(map);
 });
 
 
-
-//  // Create the popup------------------------------------------
-map.on('click', 'pop_loss', function (e) {
-    var poploss = e.features[0].properties.Pop_Loss_2010;
-    var statename = e.features[0].properties.STATE_NAME;
-    var countyname = e.features[0].properties.NAME;
-    new mapboxgl.Popup()
-        .setLngLat(e.lngLat)
-        .setHTML(statename + '<br>' 
-        + '<h2>' + countyname + '</h2>')
-        .addTo(map);
-});
-// Change the cursor to a pointer when the mouse is over 
-map.on('mouseenter', 'pop_loss', function () {
-    map.getCanvas().style.cursor = 'pointer';
+// Change the cursor to a pointer when the mouse is over the us_states_elections layer.
+map.on("mouseenter", "citibike2020", function () {
+  map.getCanvas().style.cursor = "pointer";
 });
 // Change it back to a pointer when it leaves.
-map.on('mouseleave', 'pop_loss', function () {
-    map.getCanvas().style.cursor = '';
+map.on("mouseleave", "citibike2020", function () {
+  map.getCanvas().style.cursor = "";
 });
 
 
 
 
-// map2
+
+// map2-----------------------------------------------------------------------
 var map2 = new mapboxgl.Map({
   container: 'map2',
-  style: 'mapbox://styles/nk2970/cl3zar7m8004r14n9td0sljj5',
-  zoom: 5,
-  maxZoom:9,
-  minZoom:3,
-  center: [31.829, 48.770]
+  style: 'mapbox://styles/nk2970/cl4aq1o1l000k15mzupaicgsl',
+    zoom:  10.6,
+    maxZoom:18,
+    minZoom:3,
+    center:  [-73.991, 40.748]
 });
 
 
 map2.on("load", function () {
-  let layers = map.getStyle().layers;
-  let firstSymbolId;
-  for (var i = 0; i < layers.length; i++) {
-      if (layers[i].type === 'symbol') {
-          firstSymbolId = layers[i].id;
-          break;
-      }
-  }
-
-
-
 map2.addLayer(
   {
-    id: "ukraine",
+    id: "citibike2021",
     type: "circle",
     source: {
       type: "geojson",
-      data: "data/ukraineBorderCrossings.geojson",
+      data: "data/citibikeSep2021.geojson",
     },
-    'paint': {
-      'circle-color': '#ffd700',
-      'circle-stroke-color': '#0057b7',
-      'circle-stroke-width': 2,
-      'circle-radius': 6,
+    paint: {
+      'circle-radius': {
+        stops: [[10, 3], [16, 12]]
+        },
+        "circle-color":
+        [
+          "interpolate",
+          ["linear"],
+          ["get", "tripCount_2021_diff"],
+          -500,
+          "#008837",
+          -11,
+          "#b8e186",
+          2,
+          "#f2d0e5",
+          15,
+          "#d64fa2"
 
-      'circle-radius': ['interpolate', ['exponential', 2], ['zoom'],
-      10, ['interpolate', ['linear'], ['get', 'ENTRIES_DIFF'],
-          -1, 10,
-          -0.4, 1
-      ],
-      15, ['interpolate', ['linear'], ['get', 'ENTRIES_DIFF'],
-          -1, 25,
-          -0.4, 12
-      ]
-  ],
+        ],
+        
+        
+        "circle-stroke-color": "#ffffff",
+        "circle-stroke-width": 0.2,
+        
+        "circle-opacity":1,
+    },
+    
   },
-    // minzoom: 3,
-  },firstSymbolId
-  
+  "waterway-label"
 );
+
+
 });
 
-
-
-// popup-----------------------------------------------
-map2.on('click', 'ukraine', function (e) {
-  let countryName = e.features[0].properties.Country;
-  let englishName = e.features[0].properties['Name - English'];
-  let ukranianName = e.features[0].properties['Name - Ukrainian'];
-  
+// popup
+map2.on('click', 'citibike2021', function (e) {
+  var stationName = e.features[0].properties.station_name;
+  var tripStart = e.features[0].properties.tripCount_start2021;
+  var tripEnd = e.features[0].properties.tripCount_end2021;
+  var tripDiff = e.features[0].properties.tripCount_2021_diff;
   new mapboxgl.Popup()
       .setLngLat(e.lngLat)
-      .setHTML('<h4>' + countryName + '</h4>'
-          +'<p>English Name: <b>'+  englishName + '</b></p>'
-          + '<p>Ukraian Name: <b>' + ukranianName + '</b></p>')
+      .setHTML('<h4>'+stationName+'</h4>'
+      +'<h2>Difference: '+tripDiff+'</h2>'
+      + '<p>Starting trip#: '+tripStart+'<br>'
+      +"Ending trip#: " + tripEnd + '</p>')
       .addTo(map2);
 });
-// Change the cursor to a pointer when the mouse is over 
-map2.on('mouseenter', 'turnstileData', function () {
-  map2.getCanvas().style.cursor = 'pointer';
+
+
+// Change the cursor to a pointer when the mouse is over the us_states_elections layer.
+map2.on("mouseenter", "citibike2021", function () {
+  map2.getCanvas().style.cursor = "pointer";
 });
 // Change it back to a pointer when it leaves.
-map2.on('mouseleave', 'turnstileData', function () {
-  map2.getCanvas().style.cursor = '';
+map2.on("mouseleave", "citibike2021", function () {
+  map2.getCanvas().style.cursor = "";
 });
+
+
+// define layer names
+const layers = [
+  'Starting trips exceeding',
+  'More starting trips',
+  'More ending trips',
+  'Ending trips exceeding',
+  ];
+  const colors = [
+    "#d64fa2",
+    "#f2d0e5",
+    "#b8e186",
+    "#008837" 
+    
+  ];
+   
+  // create legend
+  const legend = document.getElementById('legend');
+   
+  layers.forEach((layer, i) => {
+  const color = colors[i];
+  const item = document.createElement('div');
+  const key = document.createElement('span');
+  key.className = 'legend-key';
+  key.style.backgroundColor = color;
+   
+  const value = document.createElement('span');
+  value.innerHTML = `${layer}`;
+  item.appendChild(key);
+  item.appendChild(value);
+  legend.appendChild(item);
+  });
+   
